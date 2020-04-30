d5f47b3e7426a89f48d8ed407e97f0f014a3b79e
6_Jia <17708656@qq.com>
2020-04-30 10:58:55 +0800

    1：支持批量采集模式与单个采集模式 2：支持float类型数据

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ class ModbusPlatform {
     this.modbus = new Modbus.client.TCP(this.socket, config.unit || 1);
     this.commands = [];
 
+    // modbus 模式 0:批量采集模式 1：单个采集模式 
+    if (!('modbus_mode' in config)) {
+      this.modbus_mode = 0;
+    } else {
+      this.modbus_mode = config.modbus_mode;
+    }
+    
+
     homebridge.on("didFinishLaunching", () => {
       this.socket.connect(this.ip);
       Log("Connecting to", this.ip.host);
@@ -63,8 +71,10 @@ class ModbusPlatform {
   }
 
   accessories(callback) {
-    if (!this.config)
+    if (!this.config){
       return;
+    }
+
     this.status = [];
     this.charList = [];
     this.minModbus = [];
@@ -96,11 +106,23 @@ class ModbusPlatform {
       return;
     }
 
-    for (let type in this.maxModbus) {
-      let idx = this.minModbus[type];
-      let count = 1+this.maxModbus[type]-idx;
-      this.commands.push({"cmd":'r', "type":type, "add":idx, "count":count});
+
+    if (this.modbus_mode == 0){
+      for (let type in this.maxModbus) {
+        let idx = this.minModbus[type];
+        let count = this.maxModbus[type]-idx+1;
+        this.commands.push({"cmd":'r', "type":type, "add":idx, "count":count});
+      }
+    }else{
+      for(var i=0; i<this.charList.length; i++){
+        let obj = this.charList[i];
+        let type = obj.type;
+        let idx = obj.add;
+        let count = obj.map.len;
+        this.commands.push({"cmd":'r', "type":type, "add":idx, "count":count});
+      }
     }
+
     this.commands.push({"cmd":'x'});
 
     this.socket.emit('modbus');
@@ -121,15 +143,21 @@ class ModbusPlatform {
     this.commandTime = Date.now();
 
     if (this.command.cmd == 'w') {
-      this.modbus[{'c':'WriteSingleCoil', 'r':'writeSingleRegister'}[this.command.type]](this.command.add-1, Math.round(this.command.val));
+      this.modbus[{'c':'WriteSingleCoil', 'r':'writeSingleRegister'}[this.command.type]](this.command.add, Math.round(this.command.val));
       this.command = null;
       if (this.commands.length)
         this.socket.emit('modbus');
     }
 
     else if (this.command.cmd == 'r') {
-      this.modbus[{'c':'readCoils', 'r':'readHoldingRegisters', 'i':'readInputRegisters'}[this.command.type]](this.command.add-1, this.command.count).then((resp) => {
-        this.status[this.command.type] = resp.response._body.valuesAsArray;
+      this.modbus[{'c':'readCoils', 'r':'readHoldingRegisters', 'i':'readInputRegisters'}[this.command.type]](this.command.add, this.command.count).then((resp) => {
+        if(this.modbus_mode==0){
+          this.status[this.command.type] = resp.response._body.valuesAsArray;
+        }else{
+          let k = this.command.type.toString()+"_"+this.command.add.toString();
+          this.status[k] = resp.response._body.valuesAsArray;
+        }
+       
         this.command = null;
         if (this.commands.length)
           this.socket.emit('modbus');
@@ -141,8 +169,17 @@ class ModbusPlatform {
 
     else if (this.command.cmd == 'x') {
       this.charList.forEach((obj) => {
-        let idx = this.minModbus[obj.type];
-        let val = this.status[obj.type][obj.add - idx];
+        let val = [];
+        if(this.modbus_mode==0){
+          let idx = this.minModbus[obj.type];
+          for (let i = 0; i < obj.map.len; i++) {
+            val.push(this.status[obj.type][obj.add - idx+i]);
+          }
+        }else{
+          let k = obj.type.toString()+"_"+obj.add.toString();
+          val = this.status[k];
+        }
+
         obj.accessory.update(obj.characteristic, val, obj.map);
       });
       this.firstInit = false;
@@ -236,6 +273,10 @@ class ModbusAccessory {
           }
           modbusMap = cfg;
         }
+        //  寄存器长度
+        if (!('len' in cfg)) {
+          modbusMap.len = 1;
+        }
 
         if ('validValues' in modbusMap) {
           characteristic.props.validValues = modbusMap.validValues;
@@ -256,10 +297,11 @@ class ModbusAccessory {
         if (!this.platform.minModbus[modbusType] || this.platform.minModbus[modbusType] > modbusAdd) {
           this.platform.minModbus[modbusType] = modbusAdd;
         }
-        if (!this.platform.maxModbus[modbusType] || this.platform.maxModbus[modbusType] < modbusAdd) {
-          this.platform.maxModbus[modbusType] = modbusAdd;
+        if (!this.platform.maxModbus[modbusType] || this.platform.maxModbus[modbusType] < modbusAdd+modbusMap.len) {
+          this.platform.maxModbus[modbusType] = modbusAdd+modbusMap.len;
         }
 
+        
         if (modbusType == 'i') {
           modbusMap.readonly = true;
         }
@@ -285,7 +327,6 @@ class ModbusAccessory {
             callback();
           });
         }
-
         this.platform.charList.push({'accessory': this, 'characteristic': characteristic, 'type': modbusType, 'add': modbusAdd, 'map': modbusMap});
       }
     });
@@ -308,9 +349,19 @@ class ModbusAccessory {
     if ('map' in map && (val.toString() in map.map)) {
       val = map.map[val.toString()];
     }
+
     if (characteristic.props.format == 'bool') {
       val = val ? true : false;
+    }else if(characteristic.props.format == 'float'){
+      let v1 = val[0]&0xFF;
+      let v2 = (val[0]>>8)&0xFF;
+      let v3 = val[1]&0xFF;
+      let v4 = (val[1]>>8)&0xFF;
+      let buffer = Buffer.from([v1,v2,v3,v4]);
+      val = buffer.readFloatLE(0)
     }
+
+
     if (val != characteristic.value) {
       Log(this.name, characteristic.displayName, characteristic.value, "=>", val);
       characteristic.updateValue(val);
